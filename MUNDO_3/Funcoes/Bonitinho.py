83d131bba6ee6d7178886d171ffb25194b01be27
joão Sátiro Bompastor <124584556+JoaoBompastor@users.noreply.github.com>
2023-03-10 15:40:03 -0300

    Delete MUNDO_3/Funcoes directory

--- MUNDO_3/Funcoes/Bonitinho.py
+++
@@ -1,8 +0,0 @@
-class bonitinho:
-    def Mensagem(mensagem:str) -> str:
-        print('-=' * 15)
-        print('{:^30}' .format(mensagem))
-        print('-=' * 15)
-
-    def Linha():
-        print('-=' * 15)
